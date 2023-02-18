56fc34498f83e9f9d8912d48095abda070ef9c30
maouille988 <120687232+maouille988@users.noreply.github.com>
2023-02-18 13:39:48 +1100

    Add files via upload

---
+++ fixer.js
@@ -0,0 +1,13 @@
+var Height = document.getElementById('banniereduhaut').clientHeight;
+var Width = document.getElementById('banniereduhaut').clientWidth;
+margin = Width/30
+
+
+window.addEventListener("scroll", function() {
+    console.log(Height+margin) 
+    if(this.scrollY > Height+margin && this.scrollY<1400 ) {
+        document.querySelector('div.fixer').classList.add('fixed');
+    } else {
+        document.querySelector('div.fixer').classList.remove('fixed');
+    }
+}, false);
