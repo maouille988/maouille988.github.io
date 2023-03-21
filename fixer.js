44d6d6a44cd112ed540c5bea87a1a139abe00346
maouille988 <120687232+maouille988@users.noreply.github.com>
2023-03-21 21:20:24 +1100

    Update fixer.js

--- fixer.js
+++ fixer.js
@@ -1,16 +1,16 @@
 //affectation des variables pour savoir le modèle de boite du titre 
-function fixer(){
-    var Height = document.querySelector('banniereduhaut').clientHeight;
-    var Width = document.querySelector('banniereduhaut').clientWidth;
-    margin = Width/30
 
-    window.addEventListener("scroll", function() {
-        console.log(Height+margin) 
-        if(this.scrollY > Height+margin && this.scrollY<1400 ) {
-            document.querySelector('div.fixer').classList.add('fixed');
-        } else {
-            document.querySelector('div.fixer').classList.remove('fixed');
-        }
-    }, false);
+var Height = document.querySelector('banniereduhaut').clientHeight;
+var Width = document.querySelector('banniereduhaut').clientWidth;
+margin = Width/30
+
+window.addEventListener("scroll", function() {
+    console.log(Height+margin) 
+    if(this.scrollY > Height+margin && this.scrollY<1400 ) {
+        document.querySelector('div.fixer').classList.add('fixed');
+    } else {
+        document.querySelector('div.fixer').classList.remove('fixed');
+    }
+}, false);
 }
     
